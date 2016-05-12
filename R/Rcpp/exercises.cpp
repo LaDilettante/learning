486fa96956e43c5a3c4c24d51e0399469f6c5c8c
Anh Le <aql3@duke.edu>
2016-05-12 10:37:40 -0400

    Rcpp learning

--- R/Rcpp/exercises.cpp
+++ R/Rcpp/exercises.cpp
@@ -7,13 +7,47 @@ NumericVector timesTwo(NumericVector x) {
   return x * 2;
 }
 
-// Test this function
-bool all(LogicalVector x) {
+// [[Rcpp::export]]
+bool allC(LogicalVector x) {
   int n = x.size();
   for (int i = 0; i < n; i++) {
-    if (x[i]) return false;
+    if (!x[i]) return false;
   }
   return true;
+}
+
+// [[Rcpp::export]]
+NumericVector cumprodC(NumericVector x) {
+  NumericVector res(x.size());
+  
+  res[0] = x[0];
+  for (int i = 1; i < x.size(); i++) {
+    res[i] = res[i - 1] * x[i];
+  }
+  return res;
+}
+
+// [[Rcpp::export]]
+NumericVector cumminC(NumericVector x) {
+  int n = x.size();
+  NumericVector res(n);
+  
+  res[0] = x[0];
+  double currentMin = res[0];
+  for (int i = 1; i < n; i++) {
+    if (x[i] < currentMin) {
+      res[i] = x[i];
+      currentMin = x[i];
+    } else {
+      res[i] = res[i - 1];
+    }
+  }
+  return res;
+}
+
+// [[Rcpp::export]]
+NumericVector cummaxC(NumericVector x) {
+  
 }
 
 // You can include R code blocks in C++ files processed with sourceCpp

f50e1bbe221fbe30a9f605611e7ee6853dbba2dc
Anh Le <aql3@duke.edu>
2016-05-28 17:16:49 -0400

    rcpp exercises

--- R/Rcpp/exercises.cpp
+++ R/Rcpp/exercises.cpp
@@ -45,16 +45,92 @@ NumericVector cumminC(NumericVector x) {
   return res;
 }
 
+
+// cummax()
 // [[Rcpp::export]]
 NumericVector cummaxC(NumericVector x) {
+  int n = x.size();
+  NumericVector res(n);
   
+  res[0] = x[0];
+  double currentMax = res[0];
+  for (int i = 1; i < n; i++) {
+    if (x[i] > currentMax) {
+      res[i] = x[i];
+      currentMax = x[i];
+    } else {
+      res[i] = res[i - 1];
+    }
+  }
+  return res;
 }
 
-// You can include R code blocks in C++ files processed with sourceCpp
-// (useful for testing and development). The R code will be automatically 
-// run after the compilation.
-//
+/*** R
+dat <- rnorm(100)
+all(cummaxC(dat) == cummax(dat))
+*/
+
+// diff
+// [[Rcpp::export]]
+NumericVector diffC(NumericVector x) {
+  int n = x.size();
+  NumericVector res(n - 1);
+  
+  for (int i = 0; i < n - 1; i++) {
+    res[i] = x[i + 1] - x[i];
+  }
+  return res;
+}
 
 /*** R
-timesTwo(42)
+dat <- rnorm(100)
+all(diffC(dat) == diff(dat))
+*/
+
+
+// range
+// [[Rcpp::export]]
+NumericVector rangeC(NumericVector x) {
+  int n = x.size();
+  NumericVector res(2);
+  
+  // Find min value
+  double currentMin = x[0];
+  double currentMax = x[0];
+  for (int i = 0; i < n; i++) {
+    currentMin = std::min(currentMin, x[i]);
+    currentMax = std::max(currentMax, x[i]);
+  }
+  
+  res[0] = currentMin;
+  res[1] = currentMax;
+  return(res);
+}
+
+/*** R
+dat <- rnorm(100)
+all(rangeC(dat) == range(dat))
+*/
+
+// var
+// [[Rcpp::export]]
+double varC(NumericVector x) {
+  // Computing shifted data algorithm (Wikipedia)
+  int n = x.size();
+  double sum = 0;
+  double sumsq = 0;
+  double k = x[0];
+  
+  for (int i = 0; i < n; i++) {
+    sum += x[i] - k;
+    sumsq += pow(x[i] - k, 2);
+  }
+  
+  double res = (sumsq - sum * sum / n) / (n - 1);
+  return res;
+}
+
+/*** R
+data <- rnorm(100)
+all.equal(varC(data), var(data))
 */
--- R/exercises.R
+++ R/exercises.R
@@ -1,10 +1,3 @@
 rm(list = ls())
 library(Rcpp)
 Rcpp::sourceCpp("Rcpp/exercises.cpp")
-
-cumprod(c(1, 4, 2, 2))
-cumprodC(c(1, 4, 2, 2))
-
-
-all(c(0, 3, 2) > 1)
-allC(c(0, 3, 2) > 1)
